87be8ad44aaa062f1c542b21338ef301d8264a32
syahyaa <syyamaliah@gmail.com>
2023-07-12 18:49:27 +0700

    finishing dashboard frontend

---
+++ ulang-app/resources/views/user/layout/main_frontend.blade.php
@@ -0,0 +1,265 @@
+<div class="main-banner" id="top">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-6">
+                    <div class="left-content">
+                        <div class="thumb">
+                            <div class="inner-content">
+                                <h4>We Are GOOD-RICE</h4>
+                                <span>Beras Pilihan Dan Terpercaya</span>
+                                <div class="main-border-button">
+                                    {{-- <a href="#">Purchase Now!</a> --}}
+                                </div>
+                            </div>
+                            <img src="{{ asset ('user/images/Beras Wangi1.png') }}" style="height:630px">
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="right-content">
+                        <div class="row">
+                            <div class="col-lg-6">
+                                <div class="right-first-image">
+                                    <div class="thumb">
+                                        <div class="inner-content">
+                                            <h4>Beras Wangi</h4>
+                                            <span>Best Rice For Beras Wangi</span>
+                                        </div>
+                                        <div class="hover-content">
+                                            <div class="inner">
+                                                <h4>Beras Wangi</h4>
+                                                <p>Beras ini bisa membuat kenyang bagi yang mengonsumsinya dan dapat memberikan manfaat karbohidrat bagi tubuh</p>
+                                                <div class="main-border-button"></div>
+                                            </div>
+                                        </div>
+                                        <img src="{{ asset ('user/images/Beras Raja.jpg') }}" style="height:300px">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-6">
+                                <div class="right-first-image">
+                                    <div class="thumb">
+                                        <div class="inner-content">
+                                            <h4>Beras Raja</h4>
+                                            <span>Best Rice For Beras Raja</span>
+                                        </div>
+                                        <div class="hover-content">
+                                            <div class="inner">
+                                                <h4>Beras Raja</h4>
+                                                <p>Beras ini bisa membuat kenyang bagi yang mengonsumsinya dan dapat memberikan manfaat karbohidrat bagi tubuh</p>
+                                                <div class="main-border-button"></div>
+                                            </div>
+                                        </div>
+                                        <img src="{{ asset ('user/images/Beras Item.jpg') }}" style="height:300px">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-6">
+                                <div class="right-first-image">
+                                    <div class="thumb">
+                                        <div class="inner-content">
+                                            <h4>Beras Item</h4>
+                                            <span>Best Rice For Beras Item</span>
+                                        </div>
+                                        <div class="hover-content">
+                                            <div class="inner">
+                                                <h4>Beras Item</h4>
+                                                <p>Beras ini bisa membuat kenyang bagi yang mengonsumsinya dan dapat memberikan manfaat karbohidrat bagi tubuh</p>
+                                                <div class="main-border-button">
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <img src="{{ asset ('user/images/Beras Wangi.jpg') }}" style="height:300px">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-6">
+                                <div class="right-first-image">
+                                    <div class="thumb">
+                                        <div class="inner-content">
+                                            <h4>Beras Wangi</h4>
+                                            <span>Best Rice For Beras Item</span>
+                                        </div>
+                                        <div class="hover-content">
+                                            <div class="inner">
+                                                <h4>Beras Wangi</h4>
+                                                <p>Beras ini bisa membuat kenyang bagi yang mengonsumsinya dan dapat memberikan manfaat karbohidrat bagi tubuh</p>
+                                                <div class="main-border-button">
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <img src="{{ asset ('user/images/Ketan Merah.jpg') }}" style="height:300px">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- ***** Main Banner Area End ***** -->
+
+    <!-- ***** Men Area Starts ***** -->
+    <section class="section" id="men">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6">
+                    <div class="section-heading">
+                        <h2>Beras Pilihan Dan Terpercaya</h2>
+                        <span>Details to details is what makes Good-Rice different from the other themes.</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="men-item-carousel">
+                        <div class="owl-men-item owl-carousel">
+                            <div class="item">
+                                <div class="thumb">
+                                    <div class="hover-content">
+                                        <ul>
+                                            <li><a href="single-product.html"><i class="fa fa-eye"></i></a></li>
+                                            <li><a href="single-product.html"><i class="fa fa-pencil"></i></a></li>
+                                            <li><a href="single-product.html"><i class="fa fa-shopping-cart"></i></a></li>
+                                        </ul>
+                                    </div>
+                                    <img src="{{ asset ('user/images/Beras Raja.jpg') }}" style="width:350px ; height:380px">
+                                </div>
+                                <div class="down-content">
+                                    <h4>Beras Raja</h4>
+                                    <span>$120.00</span>
+                                    <ul class="stars">
+                                        <li><i class="fa fa-star"></i></li>
+                                        <li><i class="fa fa-star"></i></li>
+                                        <li><i class="fa fa-star"></i></li>
+                                        <li><i class="fa fa-star"></i></li>
+                                        <li><i class="fa fa-star"></i></li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div class="item">
+                                <div class="thumb">
+                                    <div class="hover-content">
+                                        <ul>
+                                            <li><a href="single-product.html"><i class="fa fa-eye"></i></a></li>
+                                            <li><a href="single-product.html"><i class="fa fa-pencil"></i></a></li>
+                                            <li><a href="single-product.html"><i class="fa fa-shopping-cart"></i></a></li>
+                                        </ul>
+                                    </div>
+                                    <img src="{{ asset ('user/images/Ketan Merah.jpg') }}" style="width:350px ; height:380px">
+                                </div>
+                                <div class="down-content">
+                                    <h4>Ketan Merah</h4>
+                                    <span>$90.00</span>
+                                    <ul class="stars">
+                                        <li><i class="fa fa-star"></i></li>
+                                        <li><i class="fa fa-star"></i></li>
+                                        <li><i class="fa fa-star"></i></li>
+                                        <li><i class="fa fa-star"></i></li>
+                                        <li><i class="fa fa-star"></i></li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div class="item">
+                                <div class="thumb">
+                                    <div class="hover-content">
+                                        <ul>
+                                            <li><a href="single-product.html"><i class="fa fa-eye"></i></a></li>
+                                            <li><a href="single-product.html"><i class="fa fa-pencil"></i></a></li>
+                                            <li><a href="single-product.html"><i class="fa fa-shopping-cart"></i></a></li>
+                                        </ul>
+                                    </div>
+                                    <img src="{{ asset ('user/images/Beras Item.jpg') }}" style="width:350px ; height:380px">
+                                </div>
+                                <div class="down-content">
+                                    <h4>Beras Item</h4>
+                                    <span>$150.00</span>
+                                    <ul class="stars">
+                                        <li><i class="fa fa-star"></i></li>
+                                        <li><i class="fa fa-star"></i></li>
+                                        <li><i class="fa fa-star"></i></li>
+                                        <li><i class="fa fa-star"></i></li>
+                                        <li><i class="fa fa-star"></i></li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div class="item">
+                                <div class="thumb">
+                                    <div class="hover-content">
+                                        <ul>
+                                            <li><a href="single-product.html"><i class="fa fa-eye"></i></a></li>
+                                            <li><a href="single-product.html"><i class="fa fa-pencil"></i></a></li>
+                                            <li><a href="single-product.html"><i class="fa fa-shopping-cart"></i></a></li>
+                                        </ul>
+                                    </div>
+                                    <img src="{{ asset ('user/images/Beras Wangi.png') }}" style="width:350px ; height:380px">
+                                </div>
+                                <div class="down-content">
+                                    <h4>Beras Wangi</h4>
+                                    <span>$120.00</span>
+                                    <ul class="stars">
+                                        <li><i class="fa fa-star"></i></li>
+                                        <li><i class="fa fa-star"></i></li>
+                                        <li><i class="fa fa-star"></i></li>
+                                        <li><i class="fa fa-star"></i></li>
+                                        <li><i class="fa fa-star"></i></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- ***** Explore Area Starts ***** -->
+    <section class="section" id="explore">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6">
+                    <div class="left-content">
+                        <h2>Explore Our Products</h2>
+                        <span>Beras di toko kami memiliki kualitas yang sangat baik, beras yang kami jual memiliki Kandungan nutrisi yang masih sangat kaya. Beras yang  memiliki kandungan nutrisi penting, seperti magnesium, fosfor, selenium, thiamine, niasin, vitamin B6, mangan, dan serat.</span>
+                        <div class="quote">
+                            <i class="fa fa-quote-left"></i><p>Puaskan selera Anda dengan beras pilihan kami yang memiliki aroma dan rasa yang luar biasa!</p>
+                        </div>
+                        <p>Beras di sini memiliki vitamin dan mineral yang sangat dibutuhkan tubuh. Kamu bisa mendapatkan beragam nutrisi, seperti protein, vitamin B, vitamin E, zinc, magnesium, dan fosfor di dalamnya. </p>
+                        <p>Banyak sekali manfaat manfaat dari beras beras berkualitas yang kami jual, tak hanya itu beras yang di tawarkan juga sangat beragam.</p>
+                        <div class="main-border-button"></div>
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="right-content">
+                        <div class="row">
+                            <div class="col-lg-6">
+                                <div class="leather">
+                                    <h4>Best Rice</h4>
+                                    <span>Recomended Rice</span>
+                                </div>
+                            </div>
+                            <div class="col-lg-6">
+                                <div class="first-image">
+                                    <img src="{{ asset ('user/images/Beras1.jpg') }}" >
+                                </div>
+                            </div>
+                            <div class="col-lg-6">
+                                <div class="second-image">
+                                    <img src="{{ asset ('user/images/Beras2.jpg') }}" >
+                                </div>
+                            </div>
+                            <div class="col-lg-6">
+                                <div class="types">
+                                    <h4>Best Food</h4>
+                                    <span>Healthy Food</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- ***** Explore Area Ends ***** -->
