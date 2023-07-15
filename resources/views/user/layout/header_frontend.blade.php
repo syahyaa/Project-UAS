e5c472d5832f90ade7fd79f3be20392ebb9a7c1d
muchnurz24 <muchamadnurzaba24@gmail.com>
2023-07-15 23:14:07 +0700

    menambahkan file landing page

---
+++ resources/views/user/layout/header_frontend.blade.php
@@ -0,0 +1,65 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900&display=swap" rel="stylesheet">
+
+    <title>GOOD-RICE</title>
+
+
+    <!-- Additional CSS Files -->
+    <link rel="stylesheet" type="text/css" href="{{ asset ('user/css/bootstrap.min.css') }}">
+
+    <link rel="stylesheet" type="text/css" href="{{ asset ('user/css/font-awesome.css') }}">
+
+    <link rel="stylesheet" href="{{ asset ('user/css/templatemo-hexashop.css') }}">
+
+    <link rel="stylesheet" href="{{ asset ('user/css/owl-carousel.css') }}">
+
+    <link rel="stylesheet" href="{{ asset ('user/css/lightbox.css') }}">
+
+    <link rel="stylesheet" href="{{ asset ('user/css/flex-slider.css') }}">
+
+    </head>
+
+<header class="header-area header-sticky">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <nav class="main-nav">
+                        <!-- ***** Logo Start ***** -->
+                        <a href="index.html" class="logo">
+                        <img src="{{ asset ('user/images/tokoberas.png') }}" style = "height:80px"> 
+                        </a> 
+                        <!-- ***** Logo End ***** -->
+                        <!-- ***** Menu Start ***** -->
+                        <ul class="nav">
+                            <li class="scroll-to-section"><a href="#top" class="active">Home</a></li>
+                            <li class="scroll-to-section"><a href="#men">Produk</a></li>
+                            {{-- <li class="submenu"> --}}
+                            <li>
+                                <a href="javascript:;">Pages</a>
+                                {{-- <ul>
+                                    <li><a href="about.html">About Us</a></li>
+                                    <li><a href="products.html">Products</a></li>
+                                    <li><a href="single-product.html">Single Product</a></li>
+                                    <li><a href="contact.html">Contact Us</a></li>
+                                </ul> --}}
+                            </li>
+                            <li class="scroll-to-section"><a href="#explore">Explore</a></li>
+                        </ul>        
+                        <a class='menu-trigger'>
+                            <span>Menu</span>
+                        </a>
+                        <!-- ***** Menu End ***** -->
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </header>
---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use App\Http\Controllers\LandingPageController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DashboardController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::prefix('user')->group(function() {
+    Route::get('/dashboard',[DashboardController::class,'index']);
+    });
+
+Route::prefix('/')->group(function() {
+    Route::get('/dashboard',[LandingPageController::class,'index']);
+    });
